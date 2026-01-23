98e1d217408e4f93078532dba6b4d5f82e449bcc
akshithgoud-16 <akshithgoudchukka@gmail.com>
2026-01-23 22:58:43 +0530

    server port fixed

--- server/server.js
+++ server/server.js
@@ -113,6 +113,6 @@ io.on("connection", (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
